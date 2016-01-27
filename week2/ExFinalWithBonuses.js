3351f594e41a63feb596831b05b8c854368b9dfa
Shannon <shannon.dillman@gmail.com>
2016-01-27 09:25:56 -0500

    Rid of unnecessary -this- on iterators, more readable with -currentPlayer- var, added console.logs to help visualize what the game is doing

--- week2/ExFinalWithBonuses.js
+++ week2/ExFinalWithBonuses.js
@@ -12,39 +12,43 @@ function GameController(numPlayers) {
 
     for ( var playerNbr = 1; playerNbr <= numPlayers; playerNbr++) {
         this.game.push( new Player('player' + playerNbr) );
-    }  // build array players called for in numPlayers
+    } // build array of Player objects, creating user names of "player1", "player2" etc
 }
 
 GameController.prototype.takeTurn = function takeTurn() {
     var guessMe = Math.floor(Math.random() * 10) + 1; // game's number to match
-    // random number from 1 - 10
+                                                    // random number from 1 - 10
     var playerGuess; // the player's guess
+    var currentPlayer; // to simplify this.game[i], which IS the current player
     
     for (var n = 0; n <=20; n++) {                  // RUNNING 21 TURNS for testing
+    console.log('\n\n' + 'ROUND ' + n + '  Match ' + guessMe + ' to win.' + '\n')
     
-    for( this.i = 0; this.i < this.game.length; this.i++) {
-        playerGuess = this.game[this.i].takeGuess();
+    for( i = 0; i < this.game.length; i++) {
+        currentPlayer = this.game[i];
+        playerGuess = currentPlayer.takeGuess();
 
         if (playerGuess === guessMe) {   // if guesses correctly...
-            this.game[this.i].updateScores();
-            this.game[this.i].bonusLifeCredit();
-            if(this.game[this.i].highScore > this.gameHighScore) { // check for new
-                this.gameHighScore = this.game[this.i].highScore; // game high score
+            currentPlayer.updateScores();
+            currentPlayer.bonusLifeCredit();
+            console.log('Bonus credit ' + currentPlayer.userName);
+            if(currentPlayer.highScore > this.gameHighScore) { // check for new
+                this.gameHighScore = currentPlayer.highScore; // game high score
             }
-            if (this.game[this.i].extraLifeCredits === 3) {  // and if enough credits earned
-                this.game[this.i].addLives();               // add a life
-                this.game[this.i].resetExtraLifeCredits();  // then reset those credits
+            if (currentPlayer.extraLifeCredits === 3) {  // and if enough credits earned
+                currentPlayer.addLives();               // add a life
+                currentPlayer.resetExtraLifeCredits();  // then reset those credits
                                                     // reset to 0 when spent on a new life
             }
         } else {                                    // guesed wrong...
-            this.game[this.i].resetExtraLifeCredits();
-            this.game[this.i].subtractLives();
-            if (this.game[this.i].lives === 0) {
-                this.game[this.i].gameOver();
+            currentPlayer.resetExtraLifeCredits();
+            currentPlayer.subtractLives();
+            if (currentPlayer.lives === 0) {
+                currentPlayer.gameOver();
             }
         }
-    console.log('Round ' + n, this.game, '  this game high score: ', this.gameHighScore);
     }                                           // END RUNNING 21 TURNS
+    console.log('End round ' + n + '  this game high score: ', this.gameHighScore + '\n', this.game);
     }
 };
 
@@ -77,7 +81,7 @@ Player.prototype.updateScores = function updateScores() {
 Player.prototype.gameOver = function gameOver() {
     this.score = 0; // resetting score
     this.lives = 3; // resetting lives
-    console.log('Game over!');
+    console.log(this.userName + ', Game Over!');
 };
 
 Player.prototype.addLives = function addLives() {
@@ -98,14 +102,15 @@ Player.prototype.resetExtraLifeCredits = function resetExtraLifeCredits() {
 
 
 // -------- baby testing stuffs below ----------
-/*
+
 var game1 = new GameController(2);
 var game2 = new GameController(3);
 
 game1.game[1] instanceof Player;
 game1 instanceof GameController;
 
+game1.game[0].editUserName('barney');
 game1.game[1].editUserName('fred');
 
 game1.takeTurn();
-*/
+game2.takeTurn();
